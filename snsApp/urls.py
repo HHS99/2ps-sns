ae2d97611615f7b2a30362796823b4442775d21c
HAN <hs.han@2ps.co.jp>
2021-11-23 21:13:03 +0900

    login枠追加

--- snsApp/urls.py
+++ snsApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import signupFunc
+from .views import signupFunc, loginFunc
 
 urlpatterns = [
     path('signup/', signupFunc),
+    path('login/', loginFunc),
 ]
--- snsApp/views.py
+++ snsApp/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
 def signupFunc(request):
-  if request.method == "POST":
+  if request.method == 'POST':
     username = request.POST['username']
     password = request.POST['password']
     try:
@@ -14,3 +15,15 @@ def signupFunc(request):
     except IntegrityError:
         return render(request, 'signup.html', {'error':'このユーザーは登録されています'})
   return render(request, 'signup.html', {'some': 100})
+
+def loginFunc(request):
+  if request.method == 'POST':
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return render(request, 'login.html', {'context': 'login success'})
+    else:
+        return render(request, 'login.html', {'context': 'login success'})
+  return render(request, 'login.html', {'context': 'get'})
