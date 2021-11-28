d1534f866e60942ab3079a47a8b88dac70704327
mholee88 <95119903+mholee88@users.noreply.github.com>
2021-11-28 17:07:35 +0900

    createのcss修正

--- snsApp/views.py
+++ snsApp/views.py
@@ -9,65 +9,73 @@ from django.urls import reverse_lazy
 
 # Create your views here.
 
+
 def signupFunc(request):
-  if request.method == 'POST':
-    username = request.POST['username']
-    password = request.POST['password']
-    try:
-        User.objects.create_user(username, '', password)
-        return render(request, 'signup.html', {'some': 100})
-    except IntegrityError:
-        return render(request, 'signup.html', {'error':'このユーザーは登録されています'})
-  return render(request, 'signup.html', {'some': 100})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            User.objects.create_user(username, '', password)
+            return render(request, 'signup.html', {'some': 100})
+        except IntegrityError:
+            return render(request, 'signup.html', {'error': 'このユーザーは登録されています'})
+    return render(request, 'signup.html', {'some': 100})
+
 
 def loginFunc(request):
-  if request.method == 'POST':
-    email = request.POST['email']
-    password = request.POST['password']
-    username = User.objects.get(email=email.lower()).username
-    user = authenticate(request, username=username, password=password)
-    if user is not None:
-        login(request, user)
-        return redirect('list')
-    else:
-        return render(request, 'login.html', {'error':'メールアドレスまたはパスワードが異なります'})
-  return render(request, 'login.html', {})
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        username = User.objects.get(email=email.lower()).username
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('list')
+        else:
+            return render(request, 'login.html', {'error': 'メールアドレスまたはパスワードが異なります'})
+    return render(request, 'login.html', {})
+
 
 def logoutFunc(request):
-  logout(request)
-  return redirect('login')
+    logout(request)
+    return redirect('login')
+
 
 @login_required
 def listFunc(request):
-  object_list = BoardModel.objects.all()
-  return render(request, 'list.html', {'object_list': object_list})
+    object_list = BoardModel.objects.all()
+    return render(request, 'list.html', {'object_list': object_list})
+
 
 @login_required
 def detailFunc(request, pk):
-  object = get_object_or_404(BoardModel, pk=pk)
-  return render(request, 'detail.html', {'object': object})
+    object = get_object_or_404(BoardModel, pk=pk)
+    return render(request, 'detail.html', {'object': object})
+
 
 @login_required
 def goodFunc(request, pk):
-  object = get_object_or_404(BoardModel, pk=pk)
-  object.good = object.good + 1
-  object.save()
-  return redirect('list')
-
-@login_required
-def readFunc(request, pk):
-  object = get_object_or_404(BoardModel, pk=pk)
-  username = request.user.get_username()
-  if username in object.readText:
-    return redirect('list')
-  else:
-    object.read = object.read + 1
-    object.readText = object.readText + ' ' + username
+    object = get_object_or_404(BoardModel, pk=pk)
+    object.good = object.good + 1
     object.save()
     return redirect('list')
 
+
+@login_required
+def readFunc(request, pk):
+    object = get_object_or_404(BoardModel, pk=pk)
+    username = request.user.get_username()
+    if username in object.readText:
+        return redirect('list')
+    else:
+        object.read = object.read + 1
+        object.readText = object.readText + ' ' + username
+        object.save()
+        return redirect('list')
+
+
 class BoardCreate(CreateView):
-  template_name = 'create.html'
-  model = BoardModel
-  fields = ('title', 'content', 'author', 'snsImage')
-  success_url = reverse_lazy('list')
+    template_name = 'create.html'
+    model = BoardModel
+    fields = ('title', 'content', 'author', 'file')
+    success_url = reverse_lazy('list')
