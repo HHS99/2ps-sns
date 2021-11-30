d9f1fa59a21254b40e791f5d635e4fcfc196c6d1
HAN <hs.han@2ps.co.jp>
2021-12-01 00:26:19 +0900

    ファイルダウンロードできるように修正

--- snsApp/urls.py
+++ snsApp/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
-from .views import signupFunc, loginFunc, listFunc, logoutFunc, detailFunc, goodFunc, readFunc, BoardCreate
+from . import views
 
 urlpatterns = [
-    path('signup/', signupFunc, name='signup'),
-    path('login/', loginFunc, name='login'),
-    path('list/', listFunc, name='list'),
-    path('logout/', logoutFunc, name='logout'),
-    path('detail/<int:pk>', detailFunc, name='detail'),
-    path('good/<int:pk>', goodFunc, name='good'),
-    path('read/<int:pk>', readFunc, name='read'),
-    path('create/', BoardCreate.as_view(), name='create'),
+    path('signup/', views.signupFunc, name='signup'),
+    path('login/', views.loginFunc, name='login'),
+    path('list/', views.listFunc, name='list'),
+    path('logout/', views.logoutFunc, name='logout'),
+    path('detail/<int:pk>', views.detailFunc, name='detail'),
+    path('create/', views.BoardCreate.as_view(), name='create'),
 ]
